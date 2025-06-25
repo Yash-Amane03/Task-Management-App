0ab9e399060e28a3bbb45357229490778e22a3dc
yash-03 <yashamane003@gmail.com>
2025-06-25 21:19:21 +0530

    add axios instance file

---
+++ frontend/Task-Manager/src/utils/axiosInstance.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { BASE_URL } from "./apiPaths";
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000,
+  headers: {
+    "Content-type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+// Request Intercepter
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("token");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+//Reponse Interceptor
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    // handle common errors globally
+    if (error.response) {
+      if (error.response.status === 401) {
+        // redirect to login page
+        window.location.href = "/login";
+      } else if (error.response.status === 500) {
+        console.log("Server error. Please try again later.");
+      } else if (error.code === "ECONNABORATED") {
+        console.error("Request timeout. Please try again.");
+      }
+      return Promise.reject(error);
+    }
+  }
+);
+
+export default axiosInstance;
