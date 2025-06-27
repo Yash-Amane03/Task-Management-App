e340a756f9a10ea8284142828bdf7a62669cffde
yash0-3 <yashaane003@gmail.com>
2025-06-27 12:54:15 +0530

    add authControllers

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -12,9 +12,45 @@ const generateToken = (userId) => {
 // @access Public
 const registerUser = async (req, res) => {
     try {
-        
+        const {name, email, password, profileImageUrl, adminInviteToken} = req.body;
+
+        // check if user is exists
+        const userExits = await User.findOne({email});
+        if(!userExits){
+            res.status(400).json({
+                message:"User already exists"
+            })
+        }
+
+        // dteremine user role : admin if correct token is provided,oterwise Member
+        let role = "member";
+        if(adminInviteToken && adminInviteToken === process.env.ADMIN_INVITE_TOKEN){
+            role = "admin";
+        }
+
+        //Hash password
+        const salt = await User.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        //create new user
+        const user = await User.create({
+            name, email, password: hashedPassword, profileImageUrl, role, 
+        });
+
+        // return usr datawith jwt
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            emai:user.email,
+            role: user.role,
+            profileImageUrl: user.profileImageUrl,
+            token: generateToken(iser._id)
+        });
     } catch (error) {
-        
+        res.status(500).json({
+            message: "Server error",
+            error: error.message
+        })
     }
 }
 
--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,17 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
-const authRoutes = require("./routes/authRoutes")
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 // middlewares to handle cors
 app.use(
-    cors({
-        origin: process.env.CLIENT_URL || "",
-        methods : ["GET","POST","PUT","DELETE"],
-        allowedHeaders: ["content-type","Authorization"],
-
-    })
+  cors({
+    origin: process.env.CLIENT_URL || "",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["content-type", "Authorization"],
+  })
 );
 
 app.use(express.json());
@@ -23,13 +22,13 @@ app.use(express.json());
 connectDB();
 
 //Routes
-app.use('./api/auth',authRoutes);
-// app.use('./api/users',userRoutes);
-// app.use('./api/tasks',taskRoutes);
-// app.use('./api/reports',ReportRoutes);
+app.use("/api/auth", authRoutes);
+// app.use('/api/users',userRoutes);
+// app.use('/api/tasks',taskRoutes);
+// app.use('/api/reports',ReportRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,()=>{
-    console.log(`Server is runnig at port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
