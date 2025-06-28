2a7e553163e0ac9608c7944b193a16efd3e846db
yash0-3 <yashaane003@gmail.com>
2025-06-28 20:52:50 +0530

    add api paths

--- frontend/Task-Manager/src/utils/apiPaths.js
+++ frontend/Task-Manager/src/utils/apiPaths.js
@@ -1,38 +1,38 @@
-// export const BASE_URL = "http://localhost:8000";
-// export const API_PATHS = {
-//     AUTH: {
-//         REGISTER: "/api/auth/register",
-//         LOGIN: "/api/auth/Login",
-//         GET_PROFILE: "/api/auth/profile"
-//     },
+export const BASE_URL = "http://localhost:8000";
+export const API_PATHS = {
+    AUTH: {
+        REGISTER: "/api/auth/register",
+        LOGIN: "/api/auth/Login",
+        GET_PROFILE: "/api/auth/profile"
+    },
 
-//     USERS: {
-//         GET_ALL_USERS: "/api/users",
-//         GET_USER_BY_ID: (userId)=> `/api/users/${userId}`,
-//         CREATE_USER: "/api/users/",
-//         UPDATE_USER: (userId)=> `/api/users/${userId}`,
-//         DELETE_USER: (userId)=> `/api/users/${userId}`,
-//     },
+    USERS: {
+        GET_ALL_USERS: "/api/users",
+        GET_USER_BY_ID: (userId)=> `/api/users/${userId}`,
+        CREATE_USER: "/api/users/",
+        UPDATE_USER: (userId)=> `/api/users/${userId}`,
+        DELETE_USER: (userId)=> `/api/users/${userId}`,
+    },
 
-//     TASKS: {
-//         GET_DASHBOARD_DATA: "/api/tasks/dashboard-data",
-//         GET_USER_DASHBOARD_DATA: "/api/tasks/user-dashboard-data",
-//         GET_ALL_TAKS: "/api/tasks",
-//         GET_TASK_BY_ID: (userId)=> `/api/taks/${userId}`,
-//         CREATE_TASK: "/api/tasks",
-//         UPDATE_TASK: (userId)=> `/api/tasks/${userId}`,
-//         DELETE_TASK: (userId)=> `/api/tasks/${userId}`,
+    TASKS: {
+        GET_DASHBOARD_DATA: "/api/tasks/dashboard-data",
+        GET_USER_DASHBOARD_DATA: "/api/tasks/user-dashboard-data",
+        GET_ALL_TAKS: "/api/tasks",
+        GET_TASK_BY_ID: (userId)=> `/api/taks/${userId}`,
+        CREATE_TASK: "/api/tasks",
+        UPDATE_TASK: (userId)=> `/api/tasks/${userId}`,
+        DELETE_TASK: (userId)=> `/api/tasks/${userId}`,
 
-//         UPDATE_USER_TASK_STATUS: (userId)=> `/api/tasks/${userId}/status`,
-//         UPDATE_TODO_CHECKLIST: (userId)=> `/api/tasks/${userId}/todo`,
-//     },
+        UPDATE_USER_TASK_STATUS: (userId)=> `/api/tasks/${userId}/status`,
+        UPDATE_TODO_CHECKLIST: (userId)=> `/api/tasks/${userId}/todo`,
+    },
 
-//     REPORTS: {
-//         EXPORT_TASKS: "/api/reports/export/tasks",
-//         EXPORT_USERS: "/api/reports/export/users"
-//     },
+    REPORTS: {
+        EXPORT_TASKS: "/api/reports/export/tasks",
+        EXPORT_USERS: "/api/reports/export/users"
+    },
 
-//     IMAGE: {
-//         UPLOAD_IMAGE: "/api/auth/upload-image"
-//     }
-// }
+    IMAGE: {
+        UPLOAD_IMAGE: "/api/auth/upload-image"
+    }
+}
